ed6c6569b857eb2ff681243552514b747dbe1202
Eero Pukkila <eero.pukkila@codento.com>
2019-06-22 23:43:35 +0300

    Tehtävät 3.3 ja 3.4

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
 
-persons = [
+let persons = [
     {
         "name": "Dan Abramov",
         "number": "12-43-234345",
@@ -28,11 +28,26 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const person = persons.find(p => p.id === Number(req.params.id))
+    if(person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    persons = persons.filter(p => p.id !== Number(req.params.id))
+    res.status(204).end()
+})
+
 app.get('/api/info', (req, res) => {
     infoString = `Phonebook has info for ${persons.length} people\n\n${new Date}`
     res.setHeader('content-type', 'text/plain')
     res.send((infoString))
 })
+
 
 const PORT = 3001
 app.listen(PORT, () => {
