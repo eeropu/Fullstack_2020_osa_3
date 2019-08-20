283724bbacb5453f17b22343fda66e5bbe9f0a14
Eero Pukkila <eero.pukkila@codento.com>
2019-08-20 22:41:53 +0300

    frontend update

---
+++ build/precache-manifest.ccb724e3499c556d2473d84887e291f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54aeeab82e407329a92508ce7d4f93f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cd1052e48a672697239",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "bc120ca277fc438795d3",
+    "url": "/static/js/2.dbf5eb8f.chunk.js"
+  },
+  {
+    "revision": "4cd1052e48a672697239",
+    "url": "/static/js/main.728a8b23.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
